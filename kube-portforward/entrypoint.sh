45cdab26b59f26a3128b243e93b985386dfccf07
Shmuel Raichman <shmuel.raichman@driivz.com>
2024-02-08 10:19:38 +0200

    Change - added cluster

--- kube-portforward/entrypoint.sh
+++ kube-portforward/entrypoint.sh
@@ -10,3 +10,7 @@ kubectl port-forward -n argocd svc/argocd-server "9001:80" --context $DEV_CENTRA
 # # dev:west
 kubectl port-forward -n dev-apps svc/demo-lab-service "8000:8080" --context $DEV_WEST_CTX --address='0.0.0.0' &
 kubectl port-forward -n argocd svc/argocd-server "8001:80"  --context $DEV_WEST_CTX --address='0.0.0.0'
+
+# # dev:ms-cluster
+kubectl port-forward -n dev-apps svc/demo-lab-service "8032:8080" --context $DEV_MS_CLUSTER --address='0.0.0.0' &
+kubectl port-forward -n argocd svc/argocd-server "8031:80"  --context $DEV_MS_CLUSTER --address='0.0.0.0'
